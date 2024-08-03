f14c40d68c8b70b93fee8d4c4659f29de923ad66
saad30-pixel <81121444+saad30-pixel@users.noreply.github.com>
2024-08-03 19:50:09 -0400

    Initial commit

---
+++ src/Components/Textform.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react'
+
+export default function Textform(props) {
+    const handleUpClick = ()=>{
+        let newText = text.toUpperCase();
+        setText(newText)
+        props.showAlert("Converted to Uppercase!", "success");
+    }
+
+    const handleLowClick = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText)
+        props.showAlert("Converted to Lowercase!", "success");
+    }
+
+    const handleClearClick = ()=>{
+        let newText = '';
+        setText(newText);
+        props.showAlert("Text Cleared!", "success");
+    }
+
+    const speak = () => {
+        let msg = new SpeechSynthesisUtterance();
+        msg.text = text;
+        window.speechSynthesis.speak(msg);
+      }
+
+    const handleOnChange = (event)=>{
+        setText(event.target.value);    
+    }
+
+    const [text, setText] = useState('');
+  return (
+    <>
+    <div className="container" style={{color: props.mode==='dark'?'#FFFAFA':'#18191a'}}>
+        <h1>{props.heading}</h1>
+    <div className="mb-3">
+        <textarea className="form-control" style={{backgroundColor: props.mode==='dark'?'grey':'#FFFAFA', color: props.mode==='dark'?'#FFFAFA':'#18191a'}} value={text} placeholder="Enter text here" onChange={handleOnChange} id="myBox" rows="8"></textarea>
+    </div>
+        <button name="uppercase" className="btn btn-success" onClick={handleUpClick}>Convert to Uppercase</button> {""} 
+        <button name="lowercase" className="btn btn-success mx-1 my-1" onClick={handleLowClick}>Convert to Lowercase</button> {""}
+        <button name="clear" className="btn btn-success mx-1 my-1" onClick={handleClearClick}>Clear Text</button> {""}
+        <button type="submit" onClick={speak} className="btn btn-success mx-1 my-1">Speak</button>
+    </div>
+    <div className="container my-3" style={{color: props.mode==='dark'?'#FFFAFA':'#18191a'}}>
+        <h2>Your text summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
+  )
+}
