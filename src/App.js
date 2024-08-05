6fc73c40e7e3bdd273506479621c09322907446f
saad30-pixel <81121444+saad30-pixel@users.noreply.github.com>
2024-08-05 19:23:18 -0400

    Changed code formatting

--- src/App.js
+++ src/App.js
@@ -1,70 +1,78 @@
-import React, { useState } from 'react';
-import './App.css';
-import About from './Components/About';
-import Navbar from './Components/Navbar';
-import Textform from './Components/Textform';
-import Alert from './Components/Alert';
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-  } from "react-router-dom";
+import React, { useState } from "react";
+import "./App.css";
+import About from "./Components/About";
+import Navbar from "./Components/Navbar";
+import Textform from "./Components/Textform";
+import Alert from "./Components/Alert";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light"); //Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
 
-    const [mode, setMode] = useState('light'); //Whether dark mode is enabled or not
-    const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
 
-    const showAlert = (message, type)=> {
-            setAlert({
-                msg: message,
-                type: type
-            })
-            setTimeout(() => {
-                setAlert(null);
-            }, 1500);
+  const toggleMode1 = () => {
+    if (mode === "light") {
+      setMode("gray");
+      document.body.style.backgroundColor = "rgb(179 181 184)";
+      showAlert("Gray mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
     }
+  };
 
-    const toggleMode1 = ()=>{
-        if(mode === 'light'){
-            setMode('gray');
-            document.body.style.backgroundColor = 'rgb(179 181 184)';
-            showAlert("Gray mode has been enabled", "success");
-        }
-        else{
-            setMode('light');
-            document.body.style.backgroundColor = 'white'
-            showAlert("Light mode has been enabled", "success");
-        }
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "rgb(17 39 65)";
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
     }
+  };
 
-    const toggleMode = ()=>{
-        if(mode === 'light'){
-            setMode('dark');
-            document.body.style.backgroundColor = 'rgb(17 39 65)';
-            showAlert("Dark mode has been enabled", "success");
-        }
-        else{
-            setMode('light');
-            document.body.style.backgroundColor = 'white'
-            showAlert("Light mode has been enabled", "success");
-        }
-    }
-
-    return (
-        <>
-        <Router>
-            <Navbar title="Textutils" aboutText="About" mode={mode} toggleMode={toggleMode} toggleMode1={toggleMode1} />
-            <Alert alert={alert}/>
-         <div className="container my-3">
-        <Routes>
-            <Route path="/about" element={<About mode={mode}/>} />
-            <Route path="/textform" element={<Textform showAlert={showAlert} heading="Try TextUtils - Word counter, Character counter, Text to Speech" mode={mode}/>} />
-        </Routes>
-         </div>
-        </Router>
-        </>
-    );
+  return (
+    <>
+      <Router>
+        <Navbar
+          title="Textutils"
+          aboutText="About"
+          mode={mode}
+          toggleMode={toggleMode}
+          toggleMode1={toggleMode1}
+        />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route path="/about" element={<About mode={mode} />} />
+            <Route
+              path="/textform"
+              element={
+                <Textform
+                  showAlert={showAlert}
+                  heading="Try TextUtils - Word counter, Character counter, Text to Speech"
+                  mode={mode}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- src/Components/About.js
+++ src/Components/About.js
@@ -1,55 +1,106 @@
-import React from 'react'
+import React from "react";
 // , { useState }
 
 export default function About(props) {
+  // const [myStyle, setMyStyle]= useState({
+  //     color: 'black',
+  //     backgroundColor: 'white'
+  // })
 
-    // const [myStyle, setMyStyle]= useState({
-    //     color: 'black',
-    //     backgroundColor: 'white'
-    // })
+  let myStyle = {
+    color: props.mode === "dark" ? "white" : "rgb(17 39 65)",
+    backgroundColor: props.mode === "dark" ? "rgb(37 60 87)" : "white",
+    border: "1px solid",
+    borderColor: props.mode === "dark" ? "white" : "rgb(17 39 65)",
+  };
 
-    let myStyle = {
-      color: props.mode ==='dark'?'white':'rgb(17 39 65)',
-      backgroundColor: props.mode ==='dark'?'rgb(37 60 87)':'white',
-      border: '1px solid',
-      borderColor: props.mode ==='dark'?'white':'rgb(17 39 65)'
-    }
-  
   return (
-    <div className="container" style={{color: props.mode ==='dark'?'white':'rgb(17 39 65)'}}>
-        <h2>About Us</h2>
+    <div
+      className="container"
+      style={{ color: props.mode === "dark" ? "white" : "rgb(17 39 65)" }}
+    >
+      <h2>About Us</h2>
       <div className="accordion accordion-flush" id="accordionFlushExample">
-  <div className="accordion-item">
-    <h2 className="accordion-header">
-      <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-        <strong>Analyze your text</strong>
-      </button>
-    </h2>
-    <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-      <div className="accordion-body" style={myStyle}>TextUtils gives you a way to analyze your text quickly and efficiently. Be it word count, character count or typing time.</div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              style={myStyle}
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseOne"
+              aria-expanded="false"
+              aria-controls="flush-collapseOne"
+            >
+              <strong>Analyze your text</strong>
+            </button>
+          </h2>
+          <div
+            id="flush-collapseOne"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className="accordion-body" style={myStyle}>
+              TextUtils gives you a way to analyze your text quickly and
+              efficiently. Be it word count, character count or typing time.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              style={myStyle}
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseTwo"
+              aria-expanded="false"
+              aria-controls="flush-collapseTwo"
+            >
+              <strong>Free to use</strong>
+            </button>
+          </h2>
+          <div
+            id="flush-collapseTwo"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className="accordion-body" style={myStyle}>
+              TextUtils is a free character counter tool that provides instant
+              character count & word count statistics for a given text.
+              TextUtils reports the number of words and characters. Thus it is
+              suitable for writing text with word/character limit.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              style={myStyle}
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseThree"
+              aria-expanded="false"
+              aria-controls="flush-collapseThree"
+            >
+              <strong>Browser Compatible</strong>
+            </button>
+          </h2>
+          <div
+            id="flush-collapseThree"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className="accordion-body" style={myStyle}>
+              This word counter software works in any browser such as Chrome,
+              Firefox, Internet Explorer, Safari, Opera. It suits to count
+              characters in Facebook, blog, books, excel document, pdf document,
+              essays, etc.
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-  <div className="accordion-item">
-    <h2 className="accordion-header">
-      <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-        <strong>Free to use</strong>
-      </button>
-    </h2>
-    <div id="flush-collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-      <div className="accordion-body" style={myStyle}>TextUtils is a free character counter tool that provides instant character count & word count statistics for a given text. TextUtils reports the number of words and characters. Thus it is suitable for writing text with word/character limit.</div>
-    </div>
-  </div>
-  <div className="accordion-item">
-    <h2 className="accordion-header">
-      <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-        <strong>Browser Compatible</strong>
-      </button>
-    </h2>
-    <div id="flush-collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-      <div className="accordion-body" style={myStyle}>This word counter software works in any browser such as Chrome, Firefox, Internet Explorer, Safari, Opera. It suits to count characters in Facebook, blog, books, excel document, pdf document, essays, etc.</div>
-    </div>
-  </div>
-</div>
-</div>
-  )
+  );
 }
